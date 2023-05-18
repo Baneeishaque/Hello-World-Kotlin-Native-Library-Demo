93bac91722fd99ad142c5b9a0282a5930f89cd8a
Banee Ishaque K <baneeishaque@gmail.com>
2023-05-19 04:02:01 +0530

    Flutter Platform Channels : Battery Level Sample
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- Hello-World-Flutter-Windows-Using-Kotlin-Native-Library/windows/runner/flutter_window.cpp
+++ Hello-World-Flutter-Windows-Using-Kotlin-Native-Library/windows/runner/flutter_window.cpp
@@ -1,8 +1,31 @@
 #include "flutter_window.h"
 
+#include <flutter/event_channel.h>
+#include <flutter/event_sink.h>
+#include <flutter/event_stream_handler_functions.h>
+#include <flutter/method_channel.h>
+#include <flutter/standard_method_codec.h>
+#include <windows.h>
+
+#include <memory>
 #include <optional>
 
 #include "flutter/generated_plugin_registrant.h"
+
+static constexpr int kBatteryError = -1;
+static constexpr int kNoBattery = -2;
+
+static int GetBatteryLevel() {
+  SYSTEM_POWER_STATUS status;
+  if (GetSystemPowerStatus(&status) == 0) {
+    return kBatteryError;
+  } else if (status.BatteryFlag == 128) {
+    return kNoBattery;
+  } else if (status.BatteryLifePercent == 255) {
+    return kBatteryError;
+  }
+  return status.BatteryLifePercent;
+}
 
 FlutterWindow::FlutterWindow(const flutter::DartProject& project)
     : project_(project) {}
@@ -25,6 +48,28 @@ bool FlutterWindow::OnCreate() {
     return false;
   }
   RegisterPlugins(flutter_controller_->engine());
+
+  flutter::MethodChannel<> channel(
+          flutter_controller_->engine()->messenger(), "samples.flutter.io/battery",
+          &flutter::StandardMethodCodec::GetInstance());
+  channel.SetMethodCallHandler(
+          [](const flutter::MethodCall<>& call,
+             std::unique_ptr<flutter::MethodResult<>> result) {
+              if (call.method_name() == "getBatteryLevel") {
+                int battery_level = GetBatteryLevel();
+
+                if (battery_level == kBatteryError) {
+                  result->Error("UNAVAILABLE", "Battery level not available.");
+                } else if (battery_level == kNoBattery) {
+                  result->Error("NO_BATTERY", "Device does not have a battery.");
+                } else {
+                  result->Success(battery_level);
+                }
+              } else {
+                result->NotImplemented();
+              }
+          });
+
   SetChildContent(flutter_controller_->view()->GetNativeWindow());
 
   flutter_controller_->engine()->SetNextFrameCallback([&]() {
